05c6d6ab3fb526d53821b37032ee67731c75fa30
Brian Johnson <bjohnson@evolytics.com>
2025-11-14 18:22:46 -0600

    begin adding consent functionality

--- script/tracker.js
+++ script/tracker.js
@@ -1,8 +1,18 @@
-/* Lightweight tracker shim */
-window.dataLayer = window.dataLayer || [];
-window.tracker = window.tracker || {
-  push: function(obj) {
-    try { window.dataLayer.push(obj); } catch (e) { /* ignore */ }
-    try { console.log('tracker.push', obj); } catch (e) { /* ignore */ }
+(function () {
+  window.dataLayer = window.dataLayer || [];
+  window.tracker = window.tracker || {
+    push: function (obj) {
+      console.log('tracker.push', obj)
+      var lsConsent = localStorage.getItem('CONSENT') || null;
+      var allow = false;
+
+      if (lsConsent) {
+        var consent = JSON.parse(lsConsent);
+        allow = (Number(consent.analytics) === 1);
+      }
+      if (allow) {
+        window.dataLayer.push(obj);
+      }
+    }
   }
-};
+})();
